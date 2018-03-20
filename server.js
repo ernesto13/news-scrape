6beae4b2c38c7959bda4e9bdb96eae53d064f709
Ernesto Samaniego <esamaniego53@gmail.com>
2018-03-20 13:43:13 -0700

    trying to deploy to heroku

--- server.js
+++ server.js
@@ -46,10 +46,20 @@ require("./routes/index.js")(app);
 //10 mongojs f.e.
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/newsScraper", {
+//     useMongoClient: true
+// });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/newsScraper", {
+mongoose.connect(MONGODB_URI, {
     useMongoClient: true
 });
+
 
 
 
